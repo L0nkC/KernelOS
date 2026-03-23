7be23c14bfd45bcf175dacad3e24db4057b1759e
Kimi Claw <kimi@claw.local>
2026-03-23 22:45:47 +0800

    Fix black screen - map VBE framebuffer in page tables
    The VBE framebuffer at 0xE0000000 wasn't mapped, causing page faults
    when writing to the front buffer. Added page table mapping for the
    framebuffer region (page directory entry 896).

--- kernel/paging.c
+++ kernel/paging.c
@@ -1,5 +1,5 @@
 /* kernel/paging.c
- * Simple identity paging for first 16MB
+ * Paging with VBE framebuffer support
  */
 
 #include <stdint.h>
@@ -9,6 +9,7 @@ static uint32_t page_table0[1024] __attribute__((aligned(4096)));
 static uint32_t page_table1[1024] __attribute__((aligned(4096)));
 static uint32_t page_table2[1024] __attribute__((aligned(4096)));
 static uint32_t page_table3[1024] __attribute__((aligned(4096)));
+static uint32_t page_table_vbe[1024] __attribute__((aligned(4096)));
 
 extern void enable_paging(uint32_t*);
 
@@ -21,13 +22,22 @@ void paging_init(void) {
         page_table3[i] = ((i + 3072) * 4096) | 3;
     }
     
+    /* Map VBE framebuffer at 0xE0000000 (4MB) */
+    uint32_t fb_base = 0xE0000000;
+    for (int i = 0; i < 1024; i++) {
+        page_table_vbe[i] = (fb_base + i * 4096) | 3;
+    }
+    
     page_directory[0] = ((uint32_t)page_table0) | 3;
     page_directory[1] = ((uint32_t)page_table1) | 3;
     page_directory[2] = ((uint32_t)page_table2) | 3;
     page_directory[3] = ((uint32_t)page_table3) | 3;
     
+    /* Page dir entry 896 = 0xE0000000 >> 22 */
+    page_directory[896] = ((uint32_t)page_table_vbe) | 3;
+    
     for (int i = 4; i < 1024; i++) {
-        page_directory[i] = 0;
+        if (i != 896) page_directory[i] = 0;
     }
     
     enable_paging(page_directory);
