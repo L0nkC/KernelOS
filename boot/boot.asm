d56ec18784e625395ee413e4c8d65194a19f2ddd
Kimi Claw <kimi@claw.local>
2026-03-24 14:51:54 +0800

    Fix boot - multiboot header and ISO working
    Kernel now boots successfully:
    - Multiboot header properly placed in .text section
    - GRUB ISO boots without hanging
    - Direct -kernel loading works
    - GUI draws 4 windows on desktop
    
    Test: qemu-system-i386 -kernel kernelos.bin -m 64M
          qemu-system-i386 -cdrom kernelos.iso -m 64M -boot d

--- boot/boot.asm
+++ boot/boot.asm
@@ -1,4 +1,4 @@
-; boot/boot.asm - Minimal debug version
+; boot/boot.asm - With kernel_main call
 section .text
 global _start
 extern kernel_main
@@ -6,12 +6,12 @@ extern kernel_main
 ; Multiboot header
 align 4
     dd 0x1BADB002
-    dd 0x00000003  
+    dd 0x00000003
     dd -(0x1BADB002 + 0x00000003)
 
 _start:
     cli
-    mov esp, 0x120000  ; 1MB + 128KB
+    mov esp, 0x120000
     push ebx
     push eax
     call kernel_main
